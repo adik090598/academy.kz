c38ef48d278fa7a005f95bb53c74ac490f0d365b
Sanzhar Anarbay <anarbay.sanzhar1999@gmail.com>
2020-11-05 16:13:10 +0600

    Added footer and tests section

--- resources/views/modules/front/pages/welcome.blade.php
+++ resources/views/modules/front/pages/welcome.blade.php
@@ -4,7 +4,7 @@
     @endsection
 
 @section('content')
-<header class="mb-5">
+<header>
     <div class="container">
     <div class="row">
         <div class="col-xl-7 col-sm-7 col-md-7 col-lg-7">
@@ -18,26 +18,30 @@
             </div>
         </div>
         <div class="col-xl-5 col-sm-5 col-md-5 col-lg-5">
-                <img class="teacher-img" src="{{asset('modules/front/assets/img/header/teacher.png')}}" alt="Teacher">
+            <img class="img-responsive teacher-img" src="{{asset('modules/front/assets/img/header/teacher.png')}}" alt="Teacher">
         </div>
     </div>
     </div>
-    <img class="img-responsive dids-img" src="{{asset('modules/front/assets/img/header/dids.png')}}" alt="Dids">
+    <div class="float-right">
+        <img class="img-responsive dids-img" src="{{asset('modules/front/assets/img/header/dids.png')}}" alt="Dids">
+    </div>
+
 </header>
 
     <section class="students">
-         <div class="students-text">
+        <div class="students-text">
             <h1>оқушылар</h1>
-             <p>Біздің портал арқылы түрлі байқау, жарыстар мен <br>
-                 олимпиадаларға қатысып ТӨСБЕЛГІ, СЕРТИФИКАТ, <br>
-                 ДИПЛОМ, АЛҒЫС ХАТТАРҒА ие болуға мүмкіндік бар.</p>
-         </div>
+            <p>Біздің портал арқылы түрлі байқау, жарыстар мен <br>
+                олимпиадаларға қатысып ТӨСБЕЛГІ, СЕРТИФИКАТ, <br>
+                ДИПЛОМ, АЛҒЫС ХАТТАРҒА ие болуға мүмкіндік бар.</p>
+        </div>
         <img class="img-responsive student-img" src="{{asset('modules/front/assets/img/header/student.png')}}" alt="Student">
-        <img class="img-responsive trophy-img" src="{{asset('modules/front/assets/img/header/trophy.png')}}" alt="Trophy">
+        <img class="float-right trophy-img" src="{{asset('modules/front/assets/img/header/trophy.png')}}" alt="Trophy">
+
         <div class="text-center images-bottom">
-            <img class="img-responsive image-1" src="{{asset('modules/front/assets/img/header/image1.png')}}" alt="Img1">
-            <img class="img-responsive image-2" src="{{asset('modules/front/assets/img/header/image2.png')}}" alt="Img2">
-            <img class="img-responsive image-3" src="{{asset('modules/front/assets/img/header/image3.png')}}" alt="Img3">
+            <img class="img-responsive" src="{{asset('modules/front/assets/img/header/image1.png')}}" alt="Img1">
+            <img class="img-responsive" src="{{asset('modules/front/assets/img/header/image2.png')}}" alt="Img2">
+            <img class="img-responsive" src="{{asset('modules/front/assets/img/header/image3.png')}}" alt="Img3">
         </div>
     </section>
 
@@ -49,9 +53,95 @@
                         шыңдап, жеңімпаз атануға зор мүмкіндіктер бар. <br>
                         Әр педагог жеткен жетістігіне сәйкес түрлі мақтау <br>
                         қағаздары мен төсбелгімен марапатталады.</p>
+        <div class="float-right">
         <img class="teachers-image" src="{{asset('modules/front/assets/img/header/teachers-image.png')}}" alt="Teachers Image">
+        </div>
     </section>
+
     <section class="tests">
+        <div class="container" style="margin-top: 150px">
+            <div class="row">
+                <div class="col-xl-6 col-sm-6 col-md-6 col-lg-6">
+                    <h1>Тесттар</h1>
+                </div>
+                <div class="col-xl-6 col-sm-6 col-md-6 col-lg-6">
+
+                </div>
+            </div>
+        </div>
+
+        <div class="test-items" style="background-image: url({{asset('modules/front/assets/img/header/rectangle-image.png')}})">
+            <div class="float-left rectangle-image">
+                {{--<img class="img-responsive" src="{{asset('modules/front/assets/img/header/rectangle-image.png')}}" alt="Rectangle Image">--}}
+                <div class="row all-tests">
+                    <div class="col-md-6 col-lg-6">
+                        <img class="img-responsive" src="{{asset('modules/front/assets/img/header/book1.png')}}" alt="book1">
+                        <p>Мектеп» республикалық <br>
+                            ғылыми-әдістемелік,<br>
+                            педагогикалық журнал</p>
+                    </div>
+                    <div class="col-md-6 col-lg-6">
+                        <img class="img-responsive" src="{{asset('modules/front/assets/img/header/book2.png')}}" alt="book2">
+                        <p>«Жас ғалым» дарында <br>
+                            балаларға арналған <br>
+                            республикалық <br>
+                            ғылыми-танымдық журнал</p>
+                    </div>
+                    <div class="col-md-6 col-lg-6">
+                        <img class="img-responsive" src="{{asset('modules/front/assets/img/header/book3.png')}}" alt="book3">
+                        <p> «Ардақты ардагер» <br>
+                            рухани-мәдени, <br>
+                            ақпараттық-танымдық <br>
+                            республикалық журналы</p>
+                    </div>
+                    <div class="col-md-6 col-lg-6">
+                        <img class="img-responsive" src="{{asset('modules/front/assets/img/header/book4.png')}}" alt="book4">
+                        <p>«Қазығұрт.kz» <br>
+                            әдеби-көркем, <br>
+                            мәдени-әлеуметтік, <br>
+                            тарихи-танымдық журнал</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="float-right woman-img">
+            <img class="img-responsive" src="{{asset('modules/front/assets/img/header/woman-image.png')}}" alt="Woman Image">
+        </div>
+
+        <div class="materials">
+            <div class="container" style="margin-top: 100px">
+                <div class="row">
+                    <div class="col-xl-6 col-sm-6 col-md-6 col-lg-6">
+                        <h1>материал жариялау</h1>
+                    </div>
+                    <div class="col-xl-6 col-sm-6 col-md-6 col-lg-6">
+
+                    </div>
+                </div>
+                <div class="row" style="margin-top: 50px">
+                    <div class="col-md-7 col-lg-7">
+                        <div class="text-left">
+                            <h3 style="margin-bottom: 40px">Құрметті педагогтар</h3>
+                            <p>
+                                БІЗДІҢ БАСЫЛЫМДАРҒА педагогтардың патрфолиосы үшін <br>
+                                маңызды  сабақ жоспарларын, ашық сабақ, мақала, іс-шара,  ҚМЖ,<br>
+                                ОМЖ, ҰМЖ, тәрбие сағаттары мен мұғалімдер мен оқушылардың ғылыми және шығармашылық жұмыстарын, іс-тәжірибеңізді жариялауға болады.
+                                Материалыңызды  жариялап СЕРТИФИКАТ, АЛҒЫС ХАТҚА ие бола аласыз. <br>
+                                Ақпарат алу үшін: 8 778 943 19 50; <br>
+                                8707 804 86 40, <br>
+                                эл.пошта: mektep_jurnal@mail.ru
+
+                            </p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            <div class="float-right">
+                <img class="material-image img-responsive" src="{{asset('modules/front/assets/img/header/material-image.png')}}" alt="Material Image">
+            </div>
+
+        </div>
 
     </section>
 
--- resources/views/modules/front/parts/footer.blade.php
+++ resources/views/modules/front/parts/footer.blade.php
@@ -1,1 +1,22 @@
-
+<footer>
+    <div class="container" style="margin-top: 40px">
+        <div class="row">
+            <div class="col-xl-4 col-sm-4 col-md-4 col-lg-4">
+              <h5>Academi.kz</h5>
+                <span>№1 тест-центр в Казахстане</span>
+                <p style="margin-top: 20px">Lorem ipsum dolor sit amet, consectetur adipiscing elit.  Morbi vel nunc consequat, sagittis sapien eu, imperdiet nisl. Morbi aliquam, turpis in consectetur semper, erat risus vehicula mi, at rhoncus enim odio feugiat mi. Integer tellus enim, euismod sed ultricies ut, varius sit amet lectus. Nam laoreet lorem sit amet suscipit facilisis. Duis auctor bibendum justo, vel mollis metus porta in. Nullam elit libero, varius auctor venenatis sed, feugiat at felis.</p>
+            </div>
+            <div class="col-xl-4 col-sm-4 col-md-4 col-lg-4">
+                <h5>Сайт бөлімдері</h5>
+                <ul>
+                    <li><a href="#">олимпиада</a></li>
+                    <li><a href="#">журнал</a></li>
+                    <li><a href="#">курстар</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="float-right">
+        <img class="img-responsive" src="{{asset('modules/front/assets/img/header/join-us.png')}}" alt="join us">
+    </div>
+</footer>
